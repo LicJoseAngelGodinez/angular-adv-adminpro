761dc8c0e19339f2bfe10432ae5a3a52c9d9832d
José Ángel Peña Godínez <joseangel.godinez1989@gmail.com>
2020-10-18 12:45:34 -0500

    [CLASE 35] - creando pages routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,23 @@ import { NgModule } from '@angular/core';
 // MODULES
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 // COMPONENTS
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
