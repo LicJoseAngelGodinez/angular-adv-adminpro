f564c53a4480f1a40f20e4c3af8d4ef13ff919fb
José Ángel Peña Godínez <joseangel.godinez1989@gmail.com>
2020-12-13 18:27:01 -0500

    [CLASE ACCOUNT] - Se agregan eventos con vanilla js.

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  public linkTheme = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let theme = `default-dark`;
+    
+    if ( localStorage.theme ) {
+      theme = localStorage.theme;
+    }
+
+    this.linkTheme.setAttribute('href', `./assets/css/colors/${ theme }.css`);
+    
   }
 
 }
